dd6f490f7fa5219b44e4bc24b4b749ddcbb93452
glglak <karem.kamel@gmail.com>
2024-11-25 03:40:55 +0200

    fixing lint

--- OverEngineeringAnalyzer/obj/Debug/net8.0/OverEngineeringAnalyzer.AssemblyInfo.cs
+++ OverEngineeringAnalyzer/obj/Debug/net8.0/OverEngineeringAnalyzer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OverEngineeringAnalyzer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ae73752fa1d2e01aa8617720d5d8a226bb87ec6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03bc8a645f2de43ebaad8eaf30f99cdd0b1c5065")]
 [assembly: System.Reflection.AssemblyProductAttribute("OverEngineeringAnalyzer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OverEngineeringAnalyzer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
