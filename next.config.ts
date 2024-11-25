3771743917b60ba54d813a449a5061644a8a8c56
glglak <karem.kamel@gmail.com>
2024-11-25 05:23:34 +0200

    change the approach

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    API_URL: process.env.NEXT_PUBLIC_API_URL
+module.exports = {
+  output: 'export',
+  images: {
+    unoptimized: true, // GitHub Pages does not support image optimization
   },
-}
-
-module.exports = nextConfig
+};
